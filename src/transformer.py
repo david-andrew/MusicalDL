8e42ac4ed31ea79b53dc9464f4fb8b2b53acf81f
David A. Samson <david.andrew.engineer@gmail.com>
2019-05-15 02:29:41 -0400

    created more complex transformer network for generating mel-spectrograms

--- src/transformer.py
+++ src/transformer.py
@@ -16,7 +16,7 @@ import pdb
 
 
 
-def add_conv_stage(in_channels, out_channels, kernel_size=3, stride=1, batch_norm=False):
+def add_conv(in_channels, out_channels, kernel_size=3, stride=1, batch_norm=False):
     padding = floor(kernel_size/2) #pad so that same size input
     
     if batch_norm:
@@ -36,11 +36,11 @@ class transformer(nn.Module):
         super(transformer, self).__init__()
 
         #create conv_stages
-        self.conv1 = add_conv_stage(in_channels, 30, batch_norm=batch_norm)
-        self.conv2 = add_conv_stage(30, 45, batch_norm=batch_norm)
-        self.conv3 = add_conv_stage(45, 60, batch_norm=batch_norm)
-        self.conv4 = add_conv_stage(60, out_channels, batch_norm=batch_norm)
-        self.conv5 = add_conv_stage(out_channels, out_channels, batch_norm=batch_norm)
+        self.conv1 = add_conv(in_channels, 30, batch_norm=batch_norm)
+        self.conv2 = add_conv(30, 45, batch_norm=batch_norm)
+        self.conv3 = add_conv(45, 60, batch_norm=batch_norm)
+        self.conv4 = add_conv(60, out_channels, batch_norm=batch_norm)
+        self.conv5 = add_conv(out_channels, out_channels, batch_norm=batch_norm)
 
     def forward(self, x):
         x = self.conv1(x)
@@ -51,29 +51,97 @@ class transformer(nn.Module):
         x *= -1 #nv-wavenet spectograms have only negative values, so invert network output to get the negative-only
         return x
 
+def add_conv_stage(in_channels, out_channels, kernel_size=3, stride=1):
+    padding = floor(kernel_size/2) #pad so that same size input
+    return nn.Sequential(
+        nn.Conv1d(in_channels, out_channels, kernel_size=1, stride=1),
+        nn.BatchNorm1d(out_channels),
+        nn.ReLU(), #suspect LeakyReLU isn't needed because output is only positive
+        nn.Conv1d(out_channels, out_channels, kernel_size=3, stride=1, padding=padding),
+        nn.BatchNorm1d(out_channels),
+        nn.ReLU()
+    )
 
 
+class reformer(nn.Module):
+    """simple residual transformer"""
+    def __init__(self, in_channels=27, out_channels=80):
+        super(reformer, self).__init__()
+
+        self.conv1 = add_conv_stage(in_channels, out_channels)
+        self.conv2 = add_conv_stage(out_channels + in_channels, out_channels)
+        self.conv3 = add_conv_stage(out_channels + in_channels, out_channels)
+        self.conv4 = add_conv_stage(out_channels + in_channels, out_channels)
+        self.conv5 = add_conv_stage(out_channels + in_channels, out_channels)
+        self.conv6 = add_conv_stage(out_channels + in_channels, out_channels)
+
+        self.dropout = nn.Dropout(0.5)
+
+    def forward(self, x):
+        a = x
+
+        a = self.conv1(a)
+        a = torch.cat((x, a), 1)
+        a = self.dropout(a)
+        
+        a = self.conv2(a)
+        a = torch.cat((x, a), 1)
+        a = self.dropout(a)
+
+        a = self.conv3(a)
+        a = torch.cat((x, a), 1)
+        a = self.dropout(a)
+
+        a = self.conv4(a)
+        a = torch.cat((x, a), 1)
+        a = self.dropout(a)
+
+        a = self.conv5(a)
+        a = torch.cat((x, a), 1)
+        a = self.dropout(a)
+
+        a = -1 * self.conv6(a) #final layer, use output directly (negative because spectrogram is negative only)
+
+        return a
+
+
+
+
+def generate_features():
+    pitch = np.linspace(note_to_freq('C4'), note_to_freq('C5'), 80) / note_to_freq('C8')
+    volume = np.random.uniform(0.001, 0.5)
+    singer = np.random.randint(20)
+    vowel = np.random.randint(5)
+    x = np.zeros((27, 80), dtype=np.float32)
+    x[0] = pitch
+    x[1] = volume
+    x[2 + singer] = 1
+    x[22 + vowel] = 1
+    x = torch.unsqueeze(torch.tensor(x), 0)
+
+    return x
 
 def train():
     #simple training process
     model_directory = os.path.join('.', 'models', 'vocal_transformer')
 
-    model = transformer(batch_norm=True)
+    model = reformer() #transformer(batch_norm=True)
     loader = VocalSetLoader(duration=1)
     train_loader = DataLoader(loader, num_workers=1, shuffle=True, batch_size=200)
 
     optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)
     model.train()
+    i = 0 #current iteration
     for epoch in range(1000):
 
-        print('--------Epoch %d---------' % epoch)
+        print('Epoch %d -------------------------' % epoch)
         if epoch % 10 == 0:
             #save model/optimizer
             print('Saving model at epoch %d' % epoch)
             torch.save(model.state_dict(), os.path.join(model_directory, ('model_%d' % epoch)))
             torch.save(optimizer.state_dict(), os.path.join(model_directory, ('optim_%d' % epoch)))
 
-        for i, batch in enumerate(train_loader):
+        for batch in train_loader:
             model.zero_grad()
             x, y = batch
             y_hat = model(x)
@@ -85,6 +153,8 @@ def train():
             print('%d:  %f' % (i, loss.data.item()))
             with open(os.path.join(model_directory, 'loss_history.txt'), 'a') as f:
                 f.write('%s\n' % str(loss.data.item()))
+
+            i += 1 #increment iteration number
 
 
 def infer():
@@ -116,19 +186,8 @@ def synthesize():
     model.load_state_dict(torch.load(os.path.join(model_directory, 'model_430')))
     model.eval()
 
-
-
     for i in range(10):
-        pitch = np.linspace(note_to_freq('C4'), note_to_freq('C5'), 80) / note_to_freq('C8')
-        volume = np.random.uniform(0.001, 0.5)
-        singer = np.random.randint(20)
-        vowel = np.random.randint(5)
-        x = np.zeros((27, 80), dtype=np.float32)
-        x[0] = pitch
-        x[1] = volume
-        x[2 + singer] = 1
-        x[22 + vowel] = 1
-        x = torch.unsqueeze(torch.tensor(x), 0)
+        x = generate_features()
         y_hat = model(x).detach()
 
         torch.save(y_hat[0], os.path.join(model_directory, 'spec_%d.pt' % i))
@@ -138,6 +197,6 @@ def synthesize():
 
 
 if __name__ == '__main__':
-    # train()
+    train()
     #infer()
-    synthesize()
+    #synthesize()
